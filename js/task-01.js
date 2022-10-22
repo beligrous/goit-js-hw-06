2b7332133c1d31da93459b475a45aa4e8f22b1ef
beligrous <beligrous@gmail.com>
2022-10-22 20:03:54 +0300

    add task #1

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,9 @@
-const numberOfCategories = document.querySelector("#categories");
-console.log(numberOfCategories);
-console.log("Number of categories: ");
+const categoriesElem = document.querySelector("#categories");
+console.log(`Number of categories ${categoriesElem.children.length}`);
+
+const categoriesItemElem = categoriesElem.children;
+
+for (let category of categoriesItemElem) {
+  console.log("Category:", category.children[0].textContent);
+  console.log("Elements:", category.children[1].children.length);
+}
