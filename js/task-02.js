2038aa566621b30bd74ec7c9ec4fd5fcb3471b45
beligrous <beligrous@gmail.com>
2022-10-22 20:45:07 +0300

    add task #2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,22 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsElem = document.querySelector("#ingredients");
+console.log(ingredientsElem);
+
+const ingredientsElemItems = ingredients.reduce((acc, ingredient) => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = ingredient;
+  ingredientItem.classList.add("item");
+  acc.push(ingredientItem);
+  return acc;
+}, []);
+
+ingredientsElem.append(...ingredientsElemItems);
+console.log(ingredientsElem);
