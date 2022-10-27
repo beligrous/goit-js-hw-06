2be2bfaaff8fb96214f26e93ad5da697a3392306
beligrous <beligrous@gmail.com>
2022-10-27 23:04:49 +0300

    add #8

--- js/task-06.js
+++ js/task-06.js
@@ -2,7 +2,7 @@ const inputElem = document.querySelector("#validation-input");
 
 function inputValidation(e) {
   let textLength = e.currentTarget.value.length;
-  console.log(textLength);
+
   if (textLength === Number(inputElem.dataset.length)) {
     inputElem.classList.add("valid");
   } else {
@@ -11,5 +11,3 @@ function inputValidation(e) {
 }
 
 inputElem.addEventListener("blur", inputValidation);
-
-console.log(inputElem.dataset.length);
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const formElem = document.querySelector(".login-form");
 
+function onFormLoginSubmit(e) {
+  e.preventDefault();
+  const { email, password } = e.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert("Всі поля повинні бути заповнені !");
+  }
+  console.log(`Email: ${email.value}, Password:${password.value}`);
+  e.currentTarget.reset;
+}
+formElem.addEventListener("submit", onFormLoginSubmit);
